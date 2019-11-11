778da43b8513634345202d347872d1d92bcd7fc1
Tajti Bendegúz <bendeguz9ctajti@gmail.com>
2019-11-11 17:09:06 +0100

    Add get data header function

--- connection.py
+++ connection.py
@@ -7,6 +7,10 @@ def read_file(filename):
         csv_reader = csv.DictReader(csv_file)
         for line in csv_reader:
             all_data.append(dict(line))
-    return all_data
+        return all_data
 
 
+def get_data_header(filename):
+    with open(filename, 'r') as csv_file:
+        data_header = csv_file.readline()
+        return data_header.strip('\n').split(',')
