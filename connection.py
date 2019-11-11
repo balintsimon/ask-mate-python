a75f0bae69e95bc9ba8603541eef04c6ccd3c539
Tajti Bendegúz <bendeguz9ctajti@gmail.com>
2019-11-11 14:38:58 +0100

    read filefunction added

--- connection.py
+++ connection.py
@@ -2,4 +2,11 @@ import csv
 
 
 def read_file(filename):
-    with open(filename, 'r') as 
+    all_data = []
+    with open(filename, 'r') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        for line in csv_reader:
+            all_data.append(dict(line))
+    return all_data
+
+
