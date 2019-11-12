17c6782eab6b9e8c2e4808ef19a7383e8bf6fbf2
Tajti Bendegúz <bendeguz9ctajti@gmail.com>
2019-11-12 16:48:38 +0100

    question page now shows the image

--- data_manager.py
+++ data_manager.py
@@ -64,20 +64,20 @@ def get_answers_to_question(question_id, answers_file):
     return answers_to_question
 
 
-def modify_vote_story(story_id, filename, vote_method):
-    story = get_single_line_by_id(story_id, filename)
-    vote_number = int(story["vote_number"])
-    if vote_number == 0 and vote_method != "vote_up":
-        pass
-    elif vote_method == "vote_up":
-        vote_number += 1
-    elif vote_method == "vote_down":
-        vote_number -= 1
-
-    story["vote_number"] = int(vote_number)
-    story_to_update = story
-
-    return story_to_update
+# def modify_vote_story(story_id, filename, vote_method):
+#     story = get_single_line_by_id(story_id, filename)
+#     vote_number = int(story["vote_number"])
+#     if vote_number == 0 and vote_method != "vote_up":
+#         pass
+#     elif vote_method == "vote_up":
+#         vote_number += 1
+#     elif vote_method == "vote_down":
+#         vote_number -= 1
+#
+#     story["vote_number"] = int(vote_number)
+#     story_to_update = story
+#
+#     return story_to_update
 
 
 def fill_out_missing_data(new_data, filename):
