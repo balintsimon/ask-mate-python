1809c6fd781f9585025b388582b701bfb4d4cb1f
dr. Simon Bálint <balladin0@gmail.com>
2019-11-27 14:29:37 +0100

    Refactor vote on answer

--- server.py
+++ server.py
@@ -198,9 +198,11 @@ def vote_questions(vote_method, question_id):
     return redirect('/list')
 
 
-@app.route('/answer/<question_id>/<answer_id>/<vote_method>')
-def vote_answers(vote_method, answer_id, question_id):
+@app.route('/answer/<answer_id>/<vote_method>')
+def vote_answers(vote_method, answer_id):
     data_manager.vote_answer(vote_method, answer_id)
+    question = data_manager.get_answer_by_answer_id(answer_id)
+    question_id = question["question_id"]
     return redirect(url_for("manage_questions", question_id=question_id))
 
 
