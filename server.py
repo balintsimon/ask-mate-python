aa434f1e7571d9814b3e8b392708812c4c663d19
dr. Simon Bálint <balladin0@gmail.com>
2019-11-27 14:32:57 +0100

    Refactor delete answer to original spec

--- server.py
+++ server.py
@@ -201,13 +201,15 @@ def vote_questions(vote_method, question_id):
 @app.route('/answer/<answer_id>/<vote_method>')
 def vote_answers(vote_method, answer_id):
     data_manager.vote_answer(vote_method, answer_id)
-    question = data_manager.get_answer_by_answer_id(answer_id)
-    question_id = question["question_id"]
+    answer = data_manager.get_answer_by_answer_id(answer_id)
+    question_id = answer["question_id"]
     return redirect(url_for("manage_questions", question_id=question_id))
 
 
-@app.route('/answer/<question_id>/<answer_id>/delete')
-def delete_answer(question_id, answer_id):
+@app.route('/answer/<answer_id>/delete')
+def delete_answer(answer_id):
+    answer = data_manager.get_answer_by_answer_id(answer_id)
+    question_id = answer["question_id"]
     data_manager.delete_answer(answer_id)
     return redirect(url_for("manage_questions", question_id=question_id))
 
